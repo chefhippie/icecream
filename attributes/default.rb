f8c14218ab1786be0bfc366f71dacbd5bbe6b4e9
Thomas Boerger <tboerger@tbpro.de>
2013-10-17 11:54:44 +0200

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: icecream
+# Attributes:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["icecream"]["packages"] = %w(
+  icecream
+)
+
+default["icecream"]["service_name"] = "icecream"
+default["icecream"]["config_file"] = "/etc/sysconfig/icecream"
+default["icecream"]["nice_level"] = "5"
+default["icecream"]["log_file"] = "/var/log/icecream/iceccd"
+default["icecream"]["run_scheduler"] = "no"
+default["icecream"]["scheduler_log_file"] = "/var/log/icecream/scheduler"
+default["icecream"]["netname"] = "AUTOBUILD"
+default["icecream"]["scheduler_host"] = "icecream.suse.de"
+default["icecream"]["max_jobs"] = ""
+default["icecream"]["allow_remote"] = "yes"
+default["icecream"]["base_dir"] = "/var/cache/icecream"
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "icecream"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures icecream"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "icecream", "Installs/Configures icecream"
+
+supports "suse"
